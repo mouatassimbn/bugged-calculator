aa6f0be723980fa7b57d9e67a23ac01eec992da2
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-08-03 20:33:33 -0300

    Added an app related css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Button from "./components/button/button.component";
-
 function App() {
   return (
     <div className="app-wrapper light-mode">
-      <div className="app-body container p-5">
-      </div>
+      <div className="app-body"></div>
     </div>
   );
 }
