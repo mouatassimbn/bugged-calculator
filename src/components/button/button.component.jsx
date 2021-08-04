b28bfcdc87f581b01a68917921728131395e9438
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-08-03 22:21:33 -0300

    changed operation buttons into bold

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,7 +2,7 @@ const Button = ({ isOperation, text, classes = "", clickHandler }) => {
   return (
     <div className={`button-wrapper btn ${classes}`} onClick={clickHandler}>
       <div className="button-body">
-        <p className={isOperation ? "is-operation" : ""}>{text}</p>
+        <p className={isOperation ? "is-operation bold" : ""}>{text}</p>
       </div>
     </div>
   );
--- src/components/icon-button/icon-button.component.jsx
+++ src/components/icon-button/icon-button.component.jsx
@@ -3,7 +3,7 @@ import Icon from "../../utils/icons";
 const IconButton = ({ type, classes = "", isOperation }) => {
   return (
     <div className={`iconButton-wrapper btn ${classes}`}>
-      <div className={`iconButton-body ${isOperation ? "is-operation" : ""}`}>
+      <div className={`iconButton-body ${isOperation ? "is-operation bold" : ""}`}>
         {Icon(type)}
       </div>
     </div>
