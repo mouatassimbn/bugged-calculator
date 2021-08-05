ba8cca1aee2316e6d736553aaa7f689083094c08
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-08-05 18:03:33 -0300

    Made dark mode toggle functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
+import { Component } from "react";
 import ButtonsGrid from "./components/buttons-grid/buttons-grid.component";
+import ModeToggle from "./components/mode-toggle/mode-toggle.component";
 
-function App() {
-  return (
-    <div className="app-wrapper dark-mode">
-      <div className="app-body">
-        <ButtonsGrid />
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDarkMode: true,
+    };
+  }
+
+  toggleMode = () => {
+    this.setState((state) => ({ isDarkMode: !state.isDarkMode }));
+  };
+
+  render() {
+    return (
+      <div
+        className={`app-wrapper ${
+          this.state.isDarkMode ? "dark-mode" : "light-mode"
+        }`}
+      >
+        <div className="app-body container">
+          <div className="app-header">
+            <ModeToggle
+              active={this.state.isDarkMode ? "dark" : "light"}
+              lightClick={this.toggleMode}
+              darkClick={this.toggleMode}
+            />
+          </div>
+          <div className="app-screen bold">
+            <p>123.55</p>
+          </div>
+          <div className="app-content">
+            <ButtonsGrid />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/mode-toggle/mode-toggle.component.jsx
+++ src/components/mode-toggle/mode-toggle.component.jsx
@@ -1,13 +1,23 @@
 import Icon from "../../utils/icons";
 
-const ModeToggle = ({ lightClick, darkClick }) => {
+const ModeToggle = ({ active, lightClick, darkClick }) => {
   return (
     <div className="modeToggle-wrapper">
       <div className="modeToggle-body">
-        <div className="mode-button left-button" onClick={lightClick}>
+        <div
+          className={`mode-button left-button ${
+            active === "light" ? "selected" : ""
+          }`}
+          onClick={lightClick}
+        >
           {Icon("lightMode")}
         </div>
-        <div className="mode-button right-button" onClick={darkClick}>
+        <div
+          className={`mode-button right-button ${
+            active === "dark" ? "selected" : ""
+          }`}
+          onClick={darkClick}
+        >
           {Icon("darkMode")}
         </div>
       </div>
